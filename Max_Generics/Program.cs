1f95556e8f8e3a8a5054b7ac7bdf57e2f06cd931
Harshita <chandaliyaharshita222@gmail.com>
2023-05-03 19:46:03 +0530

    Find max number out of 3 Integer

--- Max_Generics/Program.cs
+++ Max_Generics/Program.cs
@@ -7,6 +7,11 @@ namespace Max_Generics
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Generics Practice Problem!");
+            Console.WriteLine("Enter 3 integer numbers");
+            int first = Convert.ToInt32(Console.ReadLine());
+            int second = Convert.ToInt32(Console.ReadLine());
+            int third = Convert.ToInt32(Console.ReadLine());
+            Find_Max.FindMaxValue(first, second, third);
         }
     }
 }
