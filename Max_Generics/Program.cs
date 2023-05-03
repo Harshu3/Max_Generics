36f62d1861fd410c7fd98e41629c6a78978f40e9
Harshita <chandaliyaharshita222@gmail.com>
2023-05-03 19:32:08 +0530

    Added Welcome Message

---
+++ Max_Generics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Max_Generics
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Generics Practice Problem!");
+        }
+    }
+}
