89fc3c2420961b315e1b37cbe3335a9912acf589
Harshita <chandaliyaharshita222@gmail.com>
2023-05-04 20:44:47 +0530

    Extend max method to take more than 3 parameters

--- Max_Generics/Find_Max.cs
+++ Max_Generics/Find_Max.cs
@@ -8,36 +8,10 @@ namespace Max_Generics
 {
     internal class Find_Max<T> where T: IComparable
     {
-        public T first, second, third;
-        public Find_Max(T first, T second, T third)
+        public T Max(params T[] a)
         {
-            this.first = first;
-            this.second = second;
-            this.third = third;
-        }
-        private static T FindMaxValue(T first, T second, T third)
-        {
-            if (first.CompareTo(second) >= 0 && first.CompareTo(third) >= 0)
-            {
-                Console.WriteLine("{0} is greater number out of {1} {2} {3}", first, first, second, third);
-                return first;
-            }
-            else if (second.CompareTo(first) >= 0 && second.CompareTo(third) >= 0)
-            {
-                Console.WriteLine("{0} is greater number out of {1} {2} {3}", second, first, second, third);
-                return second;
-            }
-            else
-            {
-                Console.WriteLine("{0} is greater number out of {1} {2} {3}", third, first, second, third);
-                return third;
-            }
-        }
-
-        public void TestMaximum()
-        {
-            T maxValue = FindMaxValue(first, second, third);
-            Console.WriteLine("Max Value: " + maxValue);
+            Array.Sort(a);
+            return a[a.Length - 1];
         }
     }
 }
--- Max_Generics/Program.cs
+++ Max_Generics/Program.cs
@@ -7,32 +7,25 @@ namespace Max_Generics
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Generics Practice Problem!");
-            Console.WriteLine("Which data type you want to check?");
+            Console.WriteLine("Please choose a data type from below");
             Console.WriteLine("1.Integer\n2.Float\n3.String");
             int option = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter 3 values");
             switch (option)
             {
                 case 1:
-                    int first = Convert.ToInt32(Console.ReadLine());
-                    int second = Convert.ToInt32(Console.ReadLine());
-                    int third = Convert.ToInt32(Console.ReadLine());
-                    Find_Max<int> find = new Find_Max<int>(first, second, third);
-                    find.TestMaximum();
+                    int[] arr = { 20, 56, 65, 36, 64 };
+                    Find_Max<int> find = new Find_Max<int>();
+                    Console.WriteLine("Maximun number is:" + find.Max(arr));
                     break;
                 case 2:
-                    float a = Convert.ToSingle(Console.ReadLine());
-                    float b = Convert.ToSingle(Console.ReadLine());
-                    float c = Convert.ToSingle(Console.ReadLine());
-                    Find_Max<float> max = new Find_Max<float>(a, b, c);
-                    max.TestMaximum();
+                    float[] ar = { 20.5f, 65.6f, 56.8f, 35.6f, 98.3f };
+                    Find_Max<float> find_ = new Find_Max<float>();
+                    Console.WriteLine("Maximun number is: " + find_.Max(ar));
                     break;
                 case 3:
-                    string x = Console.ReadLine();
-                    string y = Console.ReadLine();
-                    string z = Console.ReadLine();
-                    Find_Max<string> value = new Find_Max<string>(x, y, z);
-                    value.TestMaximum();
+                    string[] ary = { "abc", "bca", "cab", "def", "efd", "fed" };
+                    Find_Max<string> max = new Find_Max<string>();
+                    Console.WriteLine("Maximun value is:" + max.Max(ary));
                     break;
             }
         }
